73433839fb5bd9cc2f291eb930ded52a06e6aa80
ivfzhou <ivfzhou@126.com>
2024-05-15 21:51:43 +0800

    命令行和 yaml 解析

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 ivfzhou
+ * Copyright (c) 2024 ivfzhou
  * ipasigner is licensed under Mulan PSL v2.
  * You can use this software according to the terms and conditions of the Mulan PSL v2.
  * You may obtain a copy of Mulan PSL v2 at:
@@ -11,14 +11,22 @@
  */
 
 #include <iostream>
+#include <fstream>
 
+#include <yaml-cpp/yaml.h>
 #include <argparse/argparse.hpp>
 
+#include "config.hpp"
+
 int main(int argv, char *argc[]) {
+    // 解析命令行参数
     argparse::ArgumentParser argParser(
-        "ipasigner",
-        std::string("v").append(VERSION).append("+").append(BUILT_TIME));
-    argParser.add_argument("-c", "--config").help("yaml format sign config").default_value("config.yml").required();
+            "ipasigner",
+            std::string("v").append(VERSION).append("+").append(BUILT_TIME));
+    argParser.set_prefix_chars("/-");
+    argParser.add_description("Used to signing .ipa file");
+    argParser.add_epilog("Author email is ivfzhou@126.com");
+    argParser.add_argument("-c", "--config").help("yaml format sign config").required();
     try {
         argParser.parse_args(argv, argc);
     } catch (const std::exception &e) {
@@ -26,8 +34,16 @@ int main(int argv, char *argc[]) {
         std::cerr << argParser << std::endl;
         return 1;
     }
-    auto cfg = argParser.get<std::string>("--config");
+    auto cfgPath = argParser.get("-c");
+
+    // 解析 yml 数据
+    std::cout << "parse yml config" << std::endl;
+    YAML::Node node = YAML::LoadFile(cfgPath);
+    const auto &cfg = node.as<ipasigner::config>();
     std::cout << cfg << std::endl;
+
+
+    // 执行签名
 
     return 0;
 }
